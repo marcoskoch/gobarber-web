51683b09f589d1c0e0e25424555e4224e029a586
Marcos Koch <marcos_koch@hotmail.com>
2020-02-07 09:47:06 -0300

    feat: finish style signin and signup

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
 import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
