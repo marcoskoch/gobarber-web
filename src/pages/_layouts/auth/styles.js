59b581a00444f5088a68981612dc5416c2abff7c
Marcos Koch <marcos_koch@hotmail.com>
2020-02-06 23:32:31 -0300

    feat: style login page

--- src/pages/_layouts/auth/styles.js
+++ src/pages/_layouts/auth/styles.js
@@ -3,4 +3,19 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   height: 100%;
   background: linear-gradient(-90deg, #7959c1, #ab59c1);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: 315px;
+  text-align: center;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 30px;
+  }
 `;
