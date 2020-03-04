a9122aa091eecaf4bc3554ac3e22be37b18e30c1
Marcos Koch <marcos_koch@hotmail.com>
2020-03-03 23:52:55 -0300

    feat: profile screen

---
+++ src/store/modules/user/sagas.js
@@ -0,0 +1,24 @@
+import { takeLatest, put, call, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
+
+import api from '~/services/api';
+
+import { updateProfileSuccess, updateProfileFailure } from './actions';
+
+export function* updateProfile({ payload }) {
+  const { name, email, ...rest } = payload.data;
+
+  const profile = {
+    name,
+    email,
+    ...(rest.oldPassword ? rest : {}),
+  };
+
+  const response = yield call(api.put, 'user', profile);
+
+  toast.success('Perfil atualizado com sucesso.');
+
+  yield put(updateProfileSuccess(response.data));
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
