b7daefc5a5c8f0cb674e83e014d0fba9464cdc46
Marcos Koch <marcos_koch@hotmail.com>
2020-03-09 12:44:50 -0300

    feat: update users

--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -1,4 +1,4 @@
-import { takeLatest, put, call, all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
 
 import api from '~/services/api';
@@ -6,19 +6,25 @@ import api from '~/services/api';
 import { updateProfileSuccess, updateProfileFailure } from './actions';
 
 export function* updateProfile({ payload }) {
-  const { name, email, ...rest } = payload.data;
+  try {
+    const { name, email, avatar_id, ...rest } = payload.data;
 
-  const profile = {
-    name,
-    email,
-    ...(rest.oldPassword ? rest : {}),
-  };
+    const profile = {
+      name,
+      email,
+      avatar_id,
+      ...(rest.oldPassword ? rest : {}),
+    };
 
-  const response = yield call(api.put, 'user', profile);
+    const response = yield call(api.put, 'users', profile);
 
-  toast.success('Perfil atualizado com sucesso.');
+    toast.success('Perfil atualizado com sucesso!');
 
-  yield put(updateProfileSuccess(response.data));
+    yield put(updateProfileSuccess(response.data));
+  } catch (err) {
+    toast.error('Erro ao atualizar perfil, confira seus dados!');
+    yield put(updateProfileFailure());
+  }
 }
 
 export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
