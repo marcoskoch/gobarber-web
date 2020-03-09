52113e555635988a61016e23a2ac8f209f9adc28
Marcos Koch <marcos_koch@hotmail.com>
2020-03-09 13:19:20 -0300

    feat: update avatar

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -5,6 +5,8 @@ import { Form, Input } from '@rocketseat/unform';
 import { signOut } from '~/store/modules/auth/actions';
 import { updateProfileRequest } from '~/store/modules/user/actions';
 
+import AvatarInput from './AvatarInput';
+
 import { Container } from './styles';
 
 export default function Profile() {
@@ -22,6 +24,7 @@ export default function Profile() {
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
 
